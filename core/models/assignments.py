e91a37bacddf1679471ab3b40b3b3d9a7c5d5101
tarun-hegde <githubtar@gmail.com>
2024-09-22 22:49:42 +0530

    fix: student resubmit and teacher check

--- core/models/assignments.py
+++ core/models/assignments.py
@@ -65,6 +65,7 @@ class Assignment(db.Model):
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(assignment.student_id == auth_principal.student_id, 'This assignment belongs to some other student')
         assertions.assert_valid(assignment.content is not None, 'assignment with empty content cannot be submitted')
+        assertions.assert_valid(assignment.state in [AssignmentStateEnum.GRADED, AssignmentStateEnum.DRAFT], 'only a draft assignment can be submitted')
 
         assignment.teacher_id = teacher_id
         assignment.state = AssignmentStateEnum.SUBMITTED
@@ -78,6 +79,8 @@ class Assignment(db.Model):
         assignment = Assignment.get_by_id(_id)
         assertions.assert_found(assignment, 'No assignment with this id was found')
         assertions.assert_valid(grade is not None, 'assignment with empty grade cannot be graded')
+        assertions.assert_valid(assignment.teacher_id == auth_principal.teacher_id, 'This assignment belongs to some other teacher')
+        assertions.assert_valid(assignment.grade == AssignmentStateEnum.SUBMITTED, 'only a submitted assignment can be graded')
 
         assignment.grade = grade
         assignment.state = AssignmentStateEnum.GRADED
